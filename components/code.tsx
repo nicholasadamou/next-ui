76c8683443a52664466615f5d379013e5cce501b
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-11-22 07:30:40 -0400

    Fixed CopyButton positioning relative to Code block

--- components/code.tsx
+++ components/code.tsx
@@ -1,21 +1,27 @@
-import { Lang } from "shiki"
-
-import { highlight } from "@/lib/shiki"
-
 import { CopyButton } from "./copy-button"
+import {highlight} from "@/lib/shiki";
+import {Lang} from "shiki";
 
 type CodeProps = {
-  code: string
-  lang: Lang
+	code: string
+	lang: Lang
 }
 
-export const Code = async ({ code, lang }: CodeProps) => {
-  const component = await highlight(code, "github-dark", lang)
 
-  return (
-    <div className="relative">
-      <CopyButton className="absolute right-0 top-0" copyToClipboard={code} />
-      <div dangerouslySetInnerHTML={{ __html: component }} />
-    </div>
-  )
+export const Code = async ({ code, lang }: CodeProps) => {
+	const component = await highlight(code, "github-dark", lang)
+
+	return (
+		<div
+			className="relative overflow-auto"
+			style={{ maxWidth: "100%", position: "relative" }}
+		>
+			<CopyButton className="absolute right-2 top-2" copyToClipboard={code} />
+			<div
+				className="p-4"
+				style={{ whiteSpace: "pre", overflowX: "auto" }}
+				dangerouslySetInnerHTML={{ __html: component }}
+			/>
+		</div>
+	)
 }
